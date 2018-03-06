ab643807c0859e54ccbf257b628f6f8fbfe4fb33
Sevastyan Savanyuk <sevastyan@mewssystems.com>
2018-03-06 13:03:34 +0100

    Initial commit.

---
+++ app/src/main/java/savanyuk/sevastyan/sample/avd_circle/MainActivity.kt
@@ -0,0 +1,12 @@
+package savanyuk.sevastyan.sample.avd_circle
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
