c2dacabba141757e5dc26d944de87fd008c4cc04
Sevastyan Savanyuk <sevastyan@mewssystems.com>
2018-03-07 21:08:00 +0100

    Gave up on AVD.

--- app/src/main/java/savanyuk/sevastyan/sample/avd_circle/MainActivity.kt
+++ app/src/main/java/savanyuk/sevastyan/sample/avd_circle/MainActivity.kt
@@ -1,8 +1,6 @@
 package savanyuk.sevastyan.sample.avd_circle
 
-import android.animation.Animator
-import android.animation.AnimatorInflater
-import android.animation.ObjectAnimator
+import android.animation.*
 import android.graphics.drawable.AnimatedVectorDrawable
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.VectorDrawable
@@ -20,11 +18,13 @@ import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.NumberFormat
-import java.util.ArrayList
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var circle: AnimatedVectorDrawable
+    private lateinit var imageView: ImageView
     private lateinit var indicator: TextView
     private val progress = PublishSubject.create<Float>()
     private val stopTracker = PublishSubject.create<SeekBar>()
@@ -39,6 +39,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        imageView = circle_drawable
         circle = (circle_drawable as ImageView).drawable as AnimatedVectorDrawable
         indicator = progress_value_indicator
 
@@ -91,14 +92,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun animateCircle(seekBar: SeekBar) {
-        createNewAVD()
+        val newAVD = createAVD(R.drawable.vd_circle, "progress", seekBar.progress.toFloat())
+        imageView.setImageDrawable(newAVD)
+        newAVD.start()
     }
 
-    private fun createNewAVD(): AnimatedVectorDrawable {
-
-    }
-
-    private fun AnimatedVectorDrawable.create(drawableResId: Int, target: String, valueTo: Float): AnimatedVectorDrawable {
+    private fun createAVD(drawableResId: Int, target: String, valueTo: Float): AnimatedVectorDrawable {
         val avd = AnimatedVectorDrawable()
 
         // Get hold of VectorDrawable.
@@ -129,11 +128,45 @@ class MainActivity : AppCompatActivity() {
                 .set(avdState, vectorDrawable)
 
         // Get hold of the Animator (depending on the valueTo).
-        AnimatorInflater.loadAnimator(this@MainActivity, animResId)
+        val animResId = if (valueTo > .5) {
+            R.animator.circle_progress_trim_50
+        } else {
+            R.animator.circle_progress_trim_20
+        }
+        val animator = AnimatorInflater.loadAnimator(this@MainActivity, animResId)
+        avd
+                .javaClass
+                .methods
+                .find { it.name == "updateAnimatorProperty" } // as it turned out, this is very much needed, otherwise: W/PropertyValuesHolder: Method getTrimPathEnd() with type null not found on target class class android.graphics.drawable.VectorDrawable$VFullPath
+                .apply {
+                    this?.isAccessible = true
+                }
+                ?.invoke(avd, animator, target, vectorDrawable, avdState.javaClass.getDeclaredField("mShouldIgnoreInvalidAnim").apply { isAccessible = true }.get(avdState))
+                ?: updateAnimatorProperty(animator, target, vectorDrawable, valueTo)
 
-//        avdState.addTargetAnimator
+        avdState
+                .javaClass
+                .methods.also { /*it.forEach { Log.e("TEST", "method in avdState: ${it.name}") }*/ }
+                .find { it.name == "addTargetAnimator" }
+                ?.invoke(avdState, target, animator)
+
+        return avd
     }
 
+    private fun updateAnimatorProperty(animator: Animator, targetName: String,
+                                       vectorDrawable: VectorDrawable, valueTo: Float) {
+        if (animator is ObjectAnimator) {
+            // Change the property of the Animator from using reflection based on the property
+            // name to a Property object that wraps the setter and getter for modifying that
+            // specific property for a given object. By replacing the reflection with a direct call,
+            // we can largely reduce the time it takes for a animator to modify a VD property.
+            animator.setValues(PropertyValuesHolder.ofFloat("trimPathEnd", valueTo))
+        } else if (animator is AnimatorSet) {
+            for (anim in animator.childAnimations) {
+                updateAnimatorProperty(anim, targetName, vectorDrawable, valueTo)
+            }
+        }
+    }
 
     private fun AnimatedVectorDrawable.updateAnimatorTrimValue(newValue: Int) {
         val avdState = javaClass
